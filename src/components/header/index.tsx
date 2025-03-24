8ec8c8cd92d517bddeea75f6490cf0145d282b8a
Luiznunvoa <luizgpinheiro0905@gmail.com>
2025-03-24 12:11:05 -0300

     build: Inicialização do styled-components

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,9 @@
+import { StyledHeader } from "./index.styles";
+
 export function Header() {
-  return <>header</>;
+  return (
+    <StyledHeader>
+      <h1>My project</h1>
+    </StyledHeader>
+  );
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,25 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { DefaultLayout } from "../layouts/defaultLayout";
+import { DefaultLayout } from "../layouts/default";
 import { Login } from "../pages/login";
 import { Register } from "../pages/register";
+import { Home } from "../pages/home";
 
 export function BrowserRouter() {
   const router = createBrowserRouter([
     {
       element: <DefaultLayout />,
       children: [
-        { // TODO: Make it /login
-          path: "/",
+        { 
+          path: "/login",
           element: <Login />,
         },
         {
           path: "/register",
           element: <Register />,
+        },
+        {
+          path: "/",
+          element: <Home />,
         },
       ],
     },
