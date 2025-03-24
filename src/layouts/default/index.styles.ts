1de187a39d868a34db2f6b89addfe29efa8c500c
Luiznunvoa <luizgpinheiro0905@gmail.com>
2025-03-24 12:18:23 -0300

     fix: Esqueci de commitar o layout

---
+++ src/layouts/default/index.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledMain = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    padding: 10px;
+`;
